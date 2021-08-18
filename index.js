d7c19bfdc3d3cee6c808584ddf114857884cd857
Tuan Vo <tuanvo19991604@gmail.com>
2021-08-18 16:12:29 +0700

    deploy

--- index.js
+++ index.js
@@ -40,9 +40,15 @@ connectDB()
 
 const app = express()
 
-app.use(cors())
-app.use(express.json())
 
+app.use(express.json())
+app.use(cors())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/api/auth', authRouter)
 app.use('/api/product', productRouter)
 app.use('/api/category', cateRouter)
